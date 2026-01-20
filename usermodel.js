180a07799b9830c62963458ec940d2d8eb73940e
Samubhusal <samakshyabhusal066@gmail.com>
2026-01-20 22:03:40 +0545

    userSchema

--- usermodel.js
+++ usermodel.js
@@ -1,11 +1,26 @@
-const mongoose=require ("mongoose");
-mongoose.connect('mongodb://localhost:27017/mongobdPractise');
+// const mongoose=require ("mongoose");
+// mongoose.connect('mongodb://localhost:27017/mongobdPractise');
 
-const userSchema=mongoose.Schema({
-    name:String,
-    email:String,
-    nickname:String,
-    password:String,
+// const userSchema=mongoose.Schema({
+//     name:String,
+//     email:String,
+//     nickname:String,
+//     password:String,
+// });
+
+// module.exports=mongoose.model("user",userSchema);
+
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/loginsignupDB");
+
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  address: String,
+  email: String,
+  password: String, // ahile simple rakheko (no hashing)
 });
 
-module.exports=mongoose.model("user",userSchema);
+module.exports = mongoose.model("User", userSchema);
